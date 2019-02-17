440c0a56a90ba41946597dbf07cd39870a8c2c10
kasra ahmadi <kasra.research@gmail.com>
2019-02-17 15:09:42 +0330

    correction

--- models/device.js
+++ models/device.js
@@ -2,33 +2,34 @@
 var mongoose = require('mongoose');
 
 var DeviceSchema = mongoose.Schema({
-    id: {
-        type: Number,
-        unique: true,
-        required: true
-    },
-    data: [{ direction: Number,
-      time: String,
-      in_call:Array,
-      out_call_up: Array,
-      out_call_down: Array,
-      numerator:String,
-      lift_status: Number
-     }]
+	module_id: {
+		type: Number,
+		unique: true,
+		required: true
+	},
+	data: [{ direction: Number,
+	  time: String,
+	  in_call:Array,
+	  out_call_up: Array,
+	  out_call_down: Array,
+	  numerator:String,
+	  lift_status: Number,
+	  elv_id: Number
+	 }]
 })
 var device = module.exports = mongoose.model('devices', DeviceSchema);
 
 
 module.exports.addDevice = function (device, callback) {
-    device.save(callback);
+	device.save(callback);
 }
 
 module.exports.addData = function (id, newData, callback) {
-    device.findOneAndUpdate({id:id}, { $push: { data: newData } }, function (err, model) {
-        if (err) {
-            console.log(err);
-            return callback("err");
-        }
-        return callback(null);
-    });
+	device.findOneAndUpdate({id:id}, { $push: { data: newData } }, function (err, model) {
+		if (err) {
+			console.log("KIRRRRRRRRR");
+			return callback("err");
+		}
+		return callback(null);
+	});
 }
