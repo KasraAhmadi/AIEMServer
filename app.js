ede026bfaaa76bc07a2db269173f4b5f1dfcc9c2
kasra ahmadi <kasra.research@gmail.com>
2019-02-17 16:19:00 +0330

    Done

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ io.on('connection', function(socket) {
 		device.addData(Jdata.module_id, Jdata.data, function(err, model,id) {
 			if (err) {
 				if (err == "no_device") {
-					var myDevice = new device({
+					var newDevice = new device({
 						module_id: Jdata.module_id
 					});
 					device.addDevice(newDevice, function(err, model) {
